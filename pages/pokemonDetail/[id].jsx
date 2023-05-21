c80c7d0442c4dff1812ce063b4c289f6110e0b9e
shoken3207 <shoken4797@gmail.com>
2023-05-22 00:12:27 +0900

    button-style修正

--- pages/pokemonDetail/[id].jsx
+++ pages/pokemonDetail/[id].jsx
@@ -20,24 +20,21 @@ const pokemonDetail = ({
       <p>重さ: {weight}</p>
       <p>高さ: {height}</p>
       <img src={image} alt='' />
-      <Link href={`/pokemonDetail/${Number(pokemonId) + 1}`}>次へ</Link>
-      <Link href={`/pokemonDetail/${Number(pokemonId) - 1}`}>前へ</Link>
+      <SButtonGroup>
+        {Number(pokemonId) > 1 && (
+          <Link href={`/pokemonDetail/${Number(pokemonId) - 1}`}>
+            <SButton>前へ</SButton>
+          </Link>
+        )}
+        {Number(pokemonId) < pokemonCount && (
+          <Link href={`/pokemonDetail/${Number(pokemonId) + 1}`}>
+            <SButton>次へ</SButton>
+          </Link>
+        )}
+      </SButtonGroup>
     </SContainer>
   );
 };
-
-const SContainer = styled.div`
-  width: 80%;
-  max-width: 500px;
-  margin: 0 auto;
-
-  img {
-    width: 500px;
-    height: 400px;
-    border: 5px solid skyblue;
-    border-radius: 20px;
-  }
-`;
 
 export async function getStaticProps(context) {
   const id = Number(context.params.id);
@@ -72,3 +69,37 @@ export async function getStaticPaths() {
 }
 
 export default pokemonDetail;
+const SContainer = styled.div`
+  width: 80%;
+  max-width: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 500px;
+    height: 400px;
+    border: 5px solid skyblue;
+    border-radius: 20px;
+  }
+`;
+
+const SButtonGroup = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const SButton = styled.button`
+  padding: 1rem 2.5rem;
+  border: none;
+  color: white;
+  background-color: skyblue;
+  box-shadow: 8px 8px 18px -11px #777777;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
